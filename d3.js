76e7a62f588b41bbfc1993bb0d35cc1212717a22
Emmett Moore <ewmoore823@gmail.com>
2018-04-27 19:42:31 -0400

    add selecttable legend

--- d3.js
+++ d3.js
@@ -1,14 +1,13 @@
 for (i=2011; i < 2018; i++) {
     d3.json("https://s3.us-east-2.amazonaws.com/emmettwmoore-assets/league-"+i+".json",
-        buildChart)
+        buildVisualization)
 }
 
 CHART_WIDTH = 960;
-CHART_HEIGHT = 800;
-CHART_MARGIN = { top: 20, right: 80, bottom: 30, left: 50 }
+CHART_HEIGHT = 640;
+CHART_MARGIN = { top: 20, right: 170, bottom: 30, left: 50 }
 
-function buildChart(league) {
-    console.log(league);
+function buildVisualization(league) {
     year = league.year;
     var svg = d3.select(".graph-" + year);
     sizeSVG(svg);
@@ -40,7 +39,11 @@ function buildChart(league) {
         .attr("fill", "#000")
         .text("Points");
 
-    doIt(g, league.teams, xAxis, yAxis)
+    colorMap = d3.scaleOrdinal(d3.schemeCategory10);
+    colorMap.domain(league.teams.map(function(team) { return team.team_id; }));
+
+    buildChart(g, league.teams, year, colorMap, xAxis, yAxis);
+    addLegend(svg, league.teams, year, colorMap);
 }
 
 function getXAxis() {
@@ -64,9 +67,7 @@ function sizeSVG(svg) {
      ) // svg height
 }
 
-function doIt(g, teams, xAxis, yAxis) {
-    z = d3.scaleOrdinal(d3.schemeCategory10);
-    z.domain(teams.map(function(team) { return team.team_id; }));
+function buildChart(g, teams, year, colorMap, xAxis, yAxis) {
     var line = d3.line()
         .curve(d3.curveCatmullRom.alpha(0.5))
         .x(function(d) { return xAxis(d.week); })
@@ -79,14 +80,91 @@ function doIt(g, teams, xAxis, yAxis) {
 
     team.append("path")
         .attr("class", "line")
+        .attr("id", function(d) {return year +"-"+d.team_id+"-line"})
         .attr("d", function(d) { return line(d.matchups); })
         .attr("stroke-width", 1.5)
-        .style("stroke", function(d) { return z(d.team_id); });
+        .style("stroke", function(d) { return colorMap(d.team_id); });
     team.append("text")
         .attr("x", 3)
         .attr("dy", "0.35em")
         .style("font", "10px sans-serif")
         .attr("transform", function(d) { //14 == last week
-            return "translate(" + xAxis(14) + "," + yAxis(d.matchups[13].points_for) + ")"; })
-        .text(function(d) { console.log(d); return d.owner });
+            return "translate(" + xAxis(14) + "," + yAxis(d.matchups[13].points_for) + ")"; });
+}
+
+function addLegend(svg, teams, year, colorMap) {
+    players = [];
+    console.log(teams);
+    for (i=0; i< teams.length; i++) {
+        player = {
+            id: year + '-' + teams[i].team_id,
+            team_id: teams[i].team_id,
+            owner: teams[i].owner,
+            team_name: teams[i].team_name,
+        }
+        players.push(player);
+    }
+    console.log(players);
+
+    var legend = svg.selectAll('.legend')
+        .data(players);
+
+    var legendEnter = legend
+        .enter()
+        .append('g')
+        .attr('class', 'legend')
+        .attr('id',function(d){ return d.id; })
+        // onclick function to toggle off the lines
+        .on('click', function (d) {
+            // uses the opacity of the item clicked on to
+            // determine whether to turn the line on or off
+            if($(this).css("opacity") == 1){
+
+                // grab the line that has the same ID as this point
+                // along w/ "-line"  use get element cause ID has spaces
+                var elemented = document.getElementById(this.id +"-line");
+
+                d3.select(elemented)
+                    .transition()
+                    .duration(100)
+                    .style("opacity", 0);
+
+                d3.select(this)
+                    .transition()
+                    .duration(100)
+                    .style ("opacity", .2);
+            } else {
+                var elemented = document.getElementById(this.id +"-line");
+                d3.select(elemented)
+                    .style("display", "block")
+                    .transition()
+                    .duration(100)
+                    .style("opacity",1);
+            }
+        });
+
+        var lastvalues = [];
+        var legendscale= d3.scaleOrdinal()
+            .domain(lastvalues)
+            .range([0,30,60,90,120,150,180,210]);
+        legendEnter.append('circle')
+            .attr('cx', CHART_WIDTH +100)
+            .attr('cy', function(d){
+                console.log(d);
+                return CHART_HEIGHT - 150 - (25 * d.team_id);
+            })
+            .attr('r', 7)
+            .style('fill', function(d) {
+                return colorMap(d.team_id);
+            });
+
+        //add the legend text
+        legendEnter.append('text')
+            .attr('font-size', "12")
+            .attr('font-family', "Arial")
+            .attr('x', CHART_WIDTH + 115)
+            .attr('y', function(d){
+                return CHART_HEIGHT - 150  - (25 * d.team_id);
+            })
+            .text(function(d){ return d.owner; });
 }
