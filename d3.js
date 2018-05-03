73d6d84810df5556dbceb55fb671f8f8a9640075
Emmett Moore <ewmoore823@gmail.com>
2018-05-02 23:58:43 -0400

    add scatterplot for all scores

--- d3.js
+++ d3.js
@@ -7,12 +7,124 @@ CHART_WIDTH = 960;
 CHART_HEIGHT = 640;
 CHART_MARGIN = { top: 20, right: 170, bottom: 30, left: 50 }
 
+
+allScores = [];
+scoreDict = {
+    scores: allScores
+}
+setTimeout(buildAllScores, 2000);
+
+function getBestWeeks(league) {
+    if (scoreDict.league === undefined) {
+        scoreDict.league = league;
+    }
+    teams = league.teams;
+    year = league.year;
+
+    for (i=0; i < teams.length; i++) {
+        owner = teams[i].owner;
+        team_name = teams[i].team_name;
+        team_id = teams[i].team_id;
+        matchups = teams[i].matchups;
+        week = i;
+        for (j=0; j < matchups.length; j++) {
+            score = matchups[j].points_for;
+            allScores.push({
+                owner: owner,
+                team_name: team_name,
+                team_id: team_id,
+                score: score,
+                year: year,
+                week: j
+            });
+        }
+    }
+    allScores.sort(function(a, b) {
+        if (a.score < b.score) {
+            return 1;
+        }
+        return -1
+    });
+    console.log({league: league, scores: allScores});
+}
+
+function buildAllScores() {
+    svg = d3.select(".all-scores");
+    sizeSVG(svg);
+    var width = svg.attr("width") - CHART_MARGIN.left - CHART_MARGIN.right;
+    var height = svg.attr("height") - CHART_MARGIN.top - CHART_MARGIN.bottom;
+    g = svg.append("g")
+        .attr("transform",
+              "translate(" + CHART_MARGIN.left + "," + CHART_MARGIN.top + ")");
+
+    xAxis = getXAxis(1, 14*7)
+    yAxis = getYAxis()
+
+    colorMap = d3.scaleOrdinal(d3.schemeCategory10);
+    colorMap.domain(scoreDict.league.teams.map(function(team) { return team.team_id; }));
+
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xAxis))
+        .append("text")
+        .attr("y", -10)
+        .attr("x", CHART_WIDTH)
+        .attr("fill", "#000")
+        .text("Week #");
+
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(yAxis))
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("fill", "#000")
+        .text("Points");
+
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    g.selectAll(".dot")
+        .data(scoreDict.scores)
+        .enter().append("circle")
+        .attr("fill", function(d) { return colorMap(d.team_id);})
+        .attr("r", 3.5)
+        .attr("cx", function(d) {
+            console.log(d.week)
+            console.log(d.year - 2011)
+            console.log(d.week + (14 * (d.year - 2011)))
+
+            return xAxis(d.week + (14 * (d.year - 2011)));
+        })
+        .attr("cy", function(d) {
+            return yAxis(d.score);
+        })
+        .on("mouseover", function(d) {
+            div.transition()
+            .duration(200)
+            .style("opacity", .9);
+            div.html(d.owner + ":" + " " + d.score + " points<br/>"+d.team_name+" ("+d.year+")")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 40) + "px");
+        })
+        .on("mouseout", function(d) {
+            div.transition()
+            .duration(500)
+            .style("opacity", 0);
+       });
+}
+
 function buildVisualization(league) {
+    getBestWeeks(league);
     year = league.year;
     var svg = d3.select(".graph-" + year);
     sizeSVG(svg);
-    xAxis = getXAxis();
-    yAxis = getYAxis();
+
+    xAxis = getXAxis(1, 14)
+    yAxis = getYAxis()
     var width = svg.attr("width") - CHART_MARGIN.left - CHART_MARGIN.right;
     var height = svg.attr("height") - CHART_MARGIN.top - CHART_MARGIN.bottom;
     g = svg.append("g")
@@ -46,8 +158,8 @@ function buildVisualization(league) {
     addLegend(svg, league.teams, year, colorMap);
 }
 
-function getXAxis() {
-    return d3.scaleLinear().range([0, CHART_WIDTH]).domain([1, 14])
+function getXAxis(min, max) {
+    return d3.scaleLinear().range([0, CHART_WIDTH]).domain([min, max])
 }
 
 function getYAxis() {
@@ -94,7 +206,6 @@ function buildChart(g, teams, year, colorMap, xAxis, yAxis) {
 
 function addLegend(svg, teams, year, colorMap) {
     players = [];
-    console.log(teams);
     for (i=0; i< teams.length; i++) {
         player = {
             id: year + '-' + teams[i].team_id,
@@ -104,7 +215,6 @@ function addLegend(svg, teams, year, colorMap) {
         }
         players.push(player);
     }
-    console.log(players);
 
     var legend = svg.selectAll('.legend')
         .data(players);
@@ -118,7 +228,8 @@ function addLegend(svg, teams, year, colorMap) {
         .on('click', function (d) {
             // uses the opacity of the item clicked on to
             // determine whether to turn the line on or off
-            if($(this).css("opacity") == 1){
+            console.log(d);
+            if($(this).css("opacity") == 1){  // ACTUALLY uses `==`
 
                 // grab the line that has the same ID as this point
                 // along w/ "-line"  use get element cause ID has spaces
@@ -131,7 +242,7 @@ function addLegend(svg, teams, year, colorMap) {
 
                 d3.select(this)
                     .attr('fakeclass', 'fakelegend')
-                    .transition()
+                   .transition()
                     .duration(300)
                     .style ("opacity", .2);
             } else {
@@ -156,7 +267,6 @@ function addLegend(svg, teams, year, colorMap) {
         legendEnter.append('circle')
             .attr('cx', CHART_WIDTH +100)
             .attr('cy', function(d){
-                console.log(d);
                 return CHART_HEIGHT - 150 - (25 * d.team_id);
             })
             .attr('r', 7)
