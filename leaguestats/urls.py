618f717a2966c87866e21a5b2f50167e34b8b69b
Emmett Moore <ewmoore823@gmail.com>
2018-05-05 13:21:34 -0400

    set up views to redirect to league page

--- leaguestats/urls.py
+++ leaguestats/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 
 from . import views
 
+app_name="leaguestats"
+
 urlpatterns = [
-    path('', views.league_stats, name='league_stats'),
+    path('', views.home, name='home'),
+    path('get_league_page', views.get_league_page, name='get_league_page'),
+    path('leauge/<int:league_id>/start/<int:start_year>/end/<int:end_year>', views.league_stats, name='league_stats'),
 ]
--- leaguestats/views.py
+++ leaguestats/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-def league_stats(request):
-    return HttpResponse("Hello world")
+def home(request):
+    return render(request, 'leaguestats/stats.html', {})
+
+
+def get_league_page(request):
+    if request.method != "POST":
+        return HttpResponse(status=400)
+    league_id = request.POST['league_id']
+    start_year = request.POST['start_year']
+    end_year = request.POST['end_year']
+    return HttpResponseRedirect(
+        reverse(
+            'leaguestats:league_stats',
+            args=(league_id, start_year, end_year)
+        )
+    )
+
+def league_stats(request, league_id, start_year, end_year):
+    return HttpResponse(f"redirect succeeded {league_id} {start_year} {end_year}")
