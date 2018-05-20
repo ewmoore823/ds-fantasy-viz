d95d0729fdfc35e5ada255df73e404614715d808
Emmett Moore <ewmoore823@gmail.com>
2018-05-20 13:11:27 -0400

    add tabs

---
+++ leaguestats/static/leaguestats/js/stats.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#league-link").click(getLeague);
+    $("#seasons-link").click(getSeasonStats);
+    $("#teams-link").click(getTeams);
+});
+
+function getLeague() {
+    console.log('getLeagues');
+}
+
+function getSeasonStats() {
+    console.log('getSeasonStats');
+}
+
+function getTeams() {
+    console.log('getTeams');
+}
